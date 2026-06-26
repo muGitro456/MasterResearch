f7fb641be865ad8e5ac63cc965188e3cdedc41a3
muGitro456 <111853642+muGitro456@users.noreply.github.com>
2026-06-26 16:23:00 +0900

    refactor: extract logger.py from database.py

---
+++ src/logger.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import os
+
+LOG_POS: list = []
+LOG_VEL: list = []
+LOG_FIT: list = []
+
+
+def store(var: np.ndarray, target: str) -> None:
+    match target:
+        case 'p':
+            LOG_POS.append(var)
+        case 'v':
+            LOG_VEL.append(var)
+        case 'e':
+            LOG_FIT.append(var)
+        case _:
+            print("該当なし")
+
+
+def write4debug(target: str, maxgen: int, m_num: str, m_name: str) -> None:
+    new_dir_path = '../backLog/' + m_num + '_' + m_name + '/'
+    os.makedirs(new_dir_path, exist_ok=True)
+
+    match target:
+        case 'p':
+            col = ['x' + str(d) for d in range(LOG_POS[0].shape[1])]
+            row = [str(d % LOG_POS[0].shape[0]) for d in range(LOG_POS[0].shape[0] * maxgen)]
+            tmp = np.array(LOG_POS)
+            tmp = (tmp.flatten()).reshape(LOG_POS[0].shape[0] * maxgen, LOG_POS[0].shape[1]).copy()
+            df = pd.DataFrame(tmp, index=row, columns=col)
+            df.to_csv(os.path.join(new_dir_path, m_name + '_pos_SP.csv'), header=True, index=True)
+        case 'v':
+            col = ['v' + str(d) for d in range(LOG_VEL[0].shape[1])]
+            row = [str(d % LOG_VEL[0].shape[0]) for d in range(LOG_VEL[0].shape[0] * maxgen)]
+            tmp = np.array(LOG_VEL)
+            tmp = (tmp.flatten()).reshape(LOG_VEL[0].shape[0] * maxgen, LOG_VEL[0].shape[1]).copy()
+            df = pd.DataFrame(tmp, index=row, columns=col)
+            df.to_csv(os.path.join(new_dir_path, m_name + '_vel_SP.csv'), header=True, index=True)
+        case _:
+            print("Error : logger")
